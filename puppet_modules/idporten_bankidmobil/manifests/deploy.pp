700449716ca62fa95ba320b7f8fecd28f257da19
Aase Mestad <aase.mestad@difi.no>
2019-10-17 12:05:52 +0000

    2019-10-17-1205-8b943c6|PBLEID-18950: Mer puppet-ting

--- puppet_modules/idporten_bankidmobil/manifests/deploy.pp
+++ puppet_modules/idporten_bankidmobil/manifests/deploy.pp
@@ -3,7 +3,7 @@ class idporten_bankidmobil::deploy inherits idporten_bankidmobil {
   include 'difilib'
 
   difilib::spring_boot_deploy { $idporten_bankidmobil::application:
-    package       => 'no.idporten.bankid',
+    package       => 'no.idporten',
     artifact      => $idporten_bankidmobil::artifact_id,
     service_name  => $idporten_bankidmobil::service_name,
     install_dir   => "${idporten_bankidmobil::install_dir}${idporten_bankidmobil::application}",
