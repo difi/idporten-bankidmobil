028c6e33b7b226af393159a01e73a79d4912445e
Erik Stedje <Erik.Stedje@difi.no>
2019-10-17 11:31:36 +0000

    2019-10-17-1131-f40c2ab|PBLEID-18947: fikse systest deploy bankid-mobil

--- puppet_modules/idporten_bankidmobil/manifests/test_setup.pp
+++ puppet_modules/idporten_bankidmobil/manifests/test_setup.pp
@@ -3,7 +3,7 @@ class idporten_bankidmobil::test_setup inherits idporten_bankidmobil{
   include platform
   if ($platform::test_setup) {
 
-    wget::fetch { 'download_bankid-keys':
+    wget::fetch { 'download_bankidmobil-keys':
       source             => 'http://static.dmz.local/vagrant/eid/resources/ID-Porten-BINAS.bid',
       destination        => "${idporten_bankidmobil::config_root}${idporten_bankidmobil::application}/ID-Porten-BINAS.bid",
       nocheckcertificate => true,
