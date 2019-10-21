c680495393c0885a2d3572b0f1c8c1b1c50a5906
Aase Mestad <aase.mestad@difi.no>
2019-10-21 14:06:14 +0000

    2019-10-21-1406-4dd3854|PBLEID-18978: Rette feil med basic auth og bruker

--- src/main/java/no/idporten/bankid/config/SecurityConfiguration.java
+++ src/main/java/no/idporten/bankid/config/SecurityConfiguration.java
@@ -1,12 +1,35 @@
 package no.idporten.bankid.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+//    @Value("${spring.security.user.name}")
+//    private String username;
+//
+//    @Value("${spring.security.user.password}")
+//    private String password;
+
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password(passwordEncoder().encode("password"))
+                .authorities("ROLE_USER");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
