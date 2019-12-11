38108678b0a29b682fe6260c9f8079625049b9f8
Erik Stedje <Erik.Stedje@difi.no>
2019-12-11 09:01:47 +0000

    2019-12-11-0901-ef727e2|PBLEID-19288: Bankid-modul manage_bids skal bare lastes ved test_setup enabled

--- puppet_modules/idporten_bankidmobil/manifests/test_setup.pp
+++ puppet_modules/idporten_bankidmobil/manifests/test_setup.pp
@@ -1,9 +1,9 @@
 class idporten_bankidmobil::test_setup inherits idporten_bankidmobil{
 
   include platform
-  include manage_bids
-  if ($platform::test_setup) {
 
+  if ($platform::test_setup) {
+    include manage_bids
     manage_bids::bid_config { $idporten_bankidmobil::application:
       application       => $idporten_bankidmobil::application
     }
